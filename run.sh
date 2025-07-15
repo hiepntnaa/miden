632203445544b32bb615795f714fcb881409a9e1
hiepnt <hiepnt.naa@gmail.com>
2025-07-15 17:05:17 +0000

    Initial commit

---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+miden-node bundled start \
+  --rpc.url "http://0.0.0.0:57291" \
+  --data-directory "/root/miden/data" \
+  --block.interval "5s" \
+  --batch.interval "2s" \
+  --max-txs-per-batch 8 \
+  --max-batches-per-block 8
